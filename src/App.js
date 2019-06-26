b3812e6d5ff245a36672df5e222907bdde85b001
Łukasz Kulbacki <lukasz.kulbacki@datality.co>
2019-06-26 19:49:34 +0200

    Add redux devtools (chrome extension)

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 class App extends React.Component {
     render() {
