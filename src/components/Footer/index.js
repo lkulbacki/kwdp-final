7654c9566dd7121d050527ba1e9530e9566424e2
Łukasz Kulbacki <lukasz.kulbacki@datality.co>
2019-06-27 20:43:56 +0200

    Style footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ import './footer.scss';
 export class Footer extends React.Component {
     render() {
         return (
-            <div>
+            <div className="container-footer">
                 <p className="rights">All rights reserved&reg;</p>
                 <ul className="nav">
-                    <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-                    <li><NavLink exact to="/faq" activeClassName="active">FAQ</NavLink></li>
-                    <li><NavLink exact to="/regulamin" activeClassName="active">Regulamin</NavLink></li>
-                    <li><NavLink exact to="/kontakt" activeClassName="active">Kontakt</NavLink></li>
+                    <li className="nav-element"><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+                    <li className="nav-element"><NavLink exact to="/faq" activeClassName="active">FAQ</NavLink></li>
+                    <li className="nav-element"><NavLink exact to="/regulamin" activeClassName="active">Regulamin</NavLink></li>
+                    <li className="nav-element"><NavLink exact to="/kontakt" activeClassName="active">Kontakt</NavLink></li>
                 </ul>
             </div>
         );
