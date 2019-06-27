c057723d3ad4691f4f477da40a4781d001e4d827
Łukasz Kulbacki <lukasz.kulbacki@datality.co>
2019-06-27 20:33:41 +0200

    Add styling to heading

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,13 +6,13 @@ export class Header extends React.Component {
     render() {
         return (
             <div className="container-header">
-                <p className="shop-name">NAZWA SKLEPU</p>
+                <p className="shop-name">Thrift Shop</p>
                 <ul className="nav">
-                    <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-                    <li><NavLink exact to="/faq" activeClassName="active">FAQ</NavLink></li>
-                    <li><NavLink exact to="/regulamin" activeClassName="active">Regulamin</NavLink></li>
-                    <li><NavLink exact to="/kontakt" activeClassName="active">Kontakt</NavLink></li>
-                    <li><NavLink exact to="/basket" activeClassName="active">Koszyk</NavLink></li>
+                    <li className="nav-element"><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+                    <li className="nav-element"><NavLink exact to="/faq" activeClassName="active">FAQ</NavLink></li>
+                    <li className="nav-element"><NavLink exact to="/regulamin" activeClassName="active">Regulamin</NavLink></li>
+                    <li className="nav-element"><NavLink exact to="/kontakt" activeClassName="active">Kontakt</NavLink></li>
+                    <li className="nav-element"><NavLink exact to="/basket" activeClassName="active">Koszyk</NavLink></li>
                 </ul>
             </div>
         );
